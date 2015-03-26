ef653d47c6218aaf87c122249086c0292a3f8a30
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-03-26 00:46:18 -0400

    some yellow and prettier

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+
+
 function choose(categ) {
-	$('.option').each(function() {
-		$(this).removeClass('selected');
-	});
+	$('.option').each(function() { $(this).removeClass('selected'); });
 	$('#'+categ).addClass('selected');
 }
 
 $('.option').click(function() {
-	var id = $(this).attr('id') 
-	choose(id);
-	$('html,body').animate({ scrollTop: $('#'+id).offset().top }, 500);
+	var id = $(this).attr('id');
+	choose(id) 
+});
+
+$('.option').on('click', function(e) {
+	var target = $( $(this).attr('href') );
+	if (target.length) {
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: 2000}, 1000);
+	}
 });
